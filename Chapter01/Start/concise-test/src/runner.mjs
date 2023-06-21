bdff10104370c9612e20c1d2f1b81b54275d73bd
WarrenT <warrentyler1024@gmail.com>
2023-06-22 00:59:21 +0100

    Add book code with error in Listing 1-1: ERR_UNSUPPORTED_ESM_URL_SCHEME

--- Chapter01/Start/concise-test/src/runner.mjs
+++ Chapter01/Start/concise-test/src/runner.mjs
@@ -1,1 +1,6 @@
-export const run = () => console.log("hello, world!");
+import path from "path";
+
+export const run = async () => {
+  await import(path.resolve(process.cwd(), "test/tests.mjs"));
+  console.log("Test run finished");
+};
