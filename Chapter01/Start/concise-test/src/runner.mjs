36ea68bd04cd267ab4e93a2e4e32abff72a3e6ac
WarrenT <warrentyler1024@gmail.com>
2023-06-24 01:36:36 +0100

    Enhance error handling and compatibility
    Description:
    Add a try-catch block to the import statement to improve error handling
    and ensure compatibility with different versions of Node.js. This
    modification specifically addresses the previous Node.js version
    problem, where Node.js 18.12 threw an UnhandledPromiseRejectionWarning
    error without a catch block. By wrapping the import in a try-catch
    block, we can handle any potential exceptions and provide more
    consistent behavior across different Node.js versions.

--- Chapter01/Start/concise-test/src/runner.mjs
+++ Chapter01/Start/concise-test/src/runner.mjs
@@ -2,8 +2,12 @@ import path from "path";
 import { pathToFileURL } from "url";
 
 export const run = async () => {
-  await import(
-    pathToFileURL(path.resolve(process.cwd(), "test/tests.mjs")).href
-  );
+  try {
+    await import(
+      pathToFileURL(path.resolve(process.cwd(), "test/tests.mjs")).href
+    );
+  } catch (e) {
+    console.error(e);
+  }
   console.log("Test run finished");
 };
