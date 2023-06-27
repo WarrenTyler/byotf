f31046fd1d59f474bbb3af46ce23ad723cc64fc7
WarrenT <warrentyler1024@gmail.com>
2023-06-27 18:35:17 +0100

    Enhance test runner with color formatting, result tracking, and CI support
    Improve the test runner by incorporating color formatting using the `color`
    function from `colors.mjs`. Track successes and failures with `successes` and
    `failures` variables. Display counts with color formatting in the `run`
    function and exit the process with correct exit codes. Modify the `it`
    function to increment `successes` for passing tests, display formatted error
    messages for failures, and properly handle exit codes. These enhancements not
    only provide better readability and informative test execution, but also ensure
    proper support for continuous integration (CI) environments with correct exit
    codes.

--- Chapter01/Start/concise-test/src/runner.mjs
+++ Chapter01/Start/concise-test/src/runner.mjs
@@ -1,5 +1,14 @@
 import path from "path";
 import { pathToFileURL } from "url";
+import { color } from "./colors.mjs";
+
+let successes = 0;
+let failures = 0;
+
+const exitCodes = {
+  ok: 0,
+  failures: 1,
+};
 
 export const run = async () => {
   try {
@@ -9,7 +18,22 @@ export const run = async () => {
   } catch (e) {
     console.error(e);
   }
-  console.log("Test run finished");
+
+  console.log(
+    color(
+      `<green>${successes}</green> tests passed, <red>${failures}</red> tests failed.`
+    )
+  );
+  process.exit(failures !== 0 ? exitCodes.failures : exitCodes.ok);
 };
 
-export const it = (name, body) => body();
+export const it = (name, body) => {
+  try {
+    body();
+    successes++;
+  } catch (e) {
+    console.error(color(`  <red>✗</red> ${name}`));
+    console.error(e);
+    failures++;
+  }
+};
