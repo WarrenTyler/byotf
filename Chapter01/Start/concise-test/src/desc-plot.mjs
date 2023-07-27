f15516b316b3e9509c3f78d685eca7e6c95a3dc3
WarrenT <warrentyler1024@gmail.com>
2023-07-27 08:22:17 +0100

    Add drawBarPlot function for displaying bar chart
    This commit introduces the drawBarPlot function, which takes a data array and
    displays a bar chart representing the frequency of starting verbs in test case
    descriptions. The function calculates the appropriate bar lengths based on the
    data and outputs the chart in the console.
    
    The drawBarPlot function enhances the readability of the code and provides a
    reusable way to display bar charts for similar data sets.

--- Chapter01/Start/concise-test/src/desc-plot.mjs
+++ Chapter01/Start/concise-test/src/desc-plot.mjs
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 import { readFileSync } from "fs";
+import { color } from "./colors.mjs";
 
-const testFilePath = "test/tests.mjs"; // Update with your actual test file path
+const testFilePath = "test/tests.mjs";
 
 const getVerb = (description) => {
   const words = description.split(" ");
@@ -18,9 +19,7 @@ const analyzeDescriptions = (descriptions) => {
   descriptions.forEach((description) => {
     const verb = getVerb(description);
     if (verb) {
-      verbCounts[verb] = verbCounts[verb]
-        ? verbCounts[verb] + 1
-        : 1;
+      verbCounts[verb] = verbCounts[verb] ? verbCounts[verb] + 1 : 1;
     }
   });
 
@@ -37,28 +36,73 @@ const generateCsvData = (verbCounts) => {
   return csvData;
 };
 
+const drawBarPlot = (csvData) => {
+  const [header, ...dataRows] = csvData.trim().split("\n");
+
+  const data = dataRows.map((row) => {
+    const [verb, count] = row.split(",");
+    return {
+      verb,
+      count: parseInt(count),
+    };
+  });
+
+  // Get the width of the terminal (fallback to 40 if not available, 120 max)
+  const terminalWidth = Math.min(process.stdout.columns ?? 40, 120);
+
+  // Determine the maximum value in the data set
+  const maxValue = Math.max(...data.map((item) => item.count));
+
+  // Define the character to represent the bars
+  const barChar = "\u2587";
+
+  // Calculate the maximum width of the verb column
+  const maxVerbWidth = Math.max(...data.map((item) => item.verb.length));
+  const maxCountWidth = Math.max(
+    ...data.map((item) => Math.floor(Math.log10(item.count)) + 1)
+  );
+
+  // Add the header for the chart output
+  let chartOutput = color(
+    `<bold>Frequency of starting verbs in test case description:</bold>\n`
+  );
+
+  // Iterate over the data and print the bar chart
+  data.forEach((item) => {
+    // Add padding to align verb column
+    const verbPadding = " ".repeat(maxVerbWidth - item.verb.length);
+
+    const countPadding = " ".repeat(
+      maxCountWidth - (Math.floor(Math.log10(item.count)) + 1)
+    );
+
+    const verbCol = `${verbPadding + item.verb}:`; // Add the verb column to the left of the bar
+    const countCol = ` ${item.count + countPadding}`; // Add the verb column to the left of the bar
+    const maxBarWidth = terminalWidth - (verbCol.length + countCol.length + 2);
+    const barLength = Math.round((item.count / maxValue) * maxBarWidth); // Scale the bars to a fixed length
+    const bar = barChar.repeat(barLength);
+
+    // Add the bar to the chart output
+    chartOutput += color(`${verbCol} <red>${bar}</red> ${countCol}\n`);
+  });
+
+  console.log(chartOutput);
+};
+
 const main = () => {
   try {
     const testsFile = readFileSync(testFilePath, "utf8");
-    const descriptions = testsFile.match(
-      /(it|test)\s*\(\s*["'][^"']*["']/g
-    );
+    const descriptions = testsFile.match(/(it|test)\s*\(\s*["'][^"']*["']/g);
     if (descriptions) {
       const sanitizedDescriptions = descriptions.map(
-        (match) =>
-          match.match(
-            /(it|test)\s*\(\s*["']([^"']+)["']/
-          )[2]
+        (match) => match.match(/(it|test)\s*\(\s*["']([^"']+)["']/)[2]
       );
-      const verbCounts = analyzeDescriptions(
-        sanitizedDescriptions
-      );
+      const verbCounts = analyzeDescriptions(sanitizedDescriptions);
       const csvData = generateCsvData(verbCounts);
-      console.log(csvData);
+
+      drawBarPlot(csvData);
     } else {
-      console.error(
-        "No test descriptions found in the test file."
-      );
+      console.error("No test descriptions found in the test file.");
     }
   } catch (error) {
     console.error(error);
