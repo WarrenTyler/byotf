373b145a1dd3fb4415d53389a2eab93116af1040
WarrenT <warrentyler1024@gmail.com>
2023-07-31 06:25:49 +0100

    Improve error reporting with printFailure and printFailures functions
    This commit enhances the concise-test testing framework by introducing the
    printFailure and printFailures functions. These functions improve error
    reporting for failed test cases and provide more detailed error messages,
    making it easier to identify and troubleshoot issues.
    
    Introduces printFailure and printFailures functions to improve error
    reporting in concise-test.

--- my-code/ch03/concise-test/src/runner.mjs
+++ my-code/ch03/concise-test/src/runner.mjs
@@ -2,13 +2,16 @@ import path from "path";
 import { pathToFileURL } from "url";
 import { color } from "./colors.mjs";
 
-let successes = 0;
-let failures = 0;
+const tick = "\u2713";
+const cross = "\u2717";
 
 const exitCodes = {
   ok: 0,
   failures: 1,
 };
+
+let successes = 0;
+let failures = [];
 
 export const run = async () => {
   try {
@@ -18,30 +21,42 @@ export const run = async () => {
   } catch (e) {
     console.error(e);
   }
-
+  printFailures();
   console.log(
     color(
-      `<green>${successes}</green> tests passed, <red>${failures}</red> tests failed.`
+      `<green>${successes}</green> tests passed, ` +
+        `<red>${failures.length}</red> tests failed.`
     )
   );
-  process.exit(failures !== 0 ? exitCodes.failures : exitCodes.ok);
+  process.exit(failures.length > 0 ? exitCodes.failures : exitCodes.ok);
 };
 
-const tick = "\u2713";
-const cross = "\u2717";
 export const it = (name, body) => {
   try {
     body();
     console.log(color(` <green>${tick}</green> ${name}`));
     successes++;
   } catch (e) {
-    console.log(color(` <red>${cross}</red> ${name}`));
-    console.error(e);
-    failures++;
+    console.error(color(` <red>${cross}</red> ${name}`));
+    failures.push(e);
   }
 };
 
 export const describe = (name, body) => {
   console.log(name);
   body();
+};
+
+const printFailure = (e) => {
+  console.error(e);
+  console.error("");
+};
+
+const printFailures = () => {
+  if (failures.length > 0) {
+    console.error("");
+    console.error("Failures:");
+    console.error("");
+  }
+  failures.forEach(printFailure);
 };
